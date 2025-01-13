1b2662e2e9495963a9f73575f61d8c423e3e6940
paul@vontos.co <paul@vontos.co>
2025-01-13 16:58:57 +0800

    update supabase-url and key

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -39,9 +39,9 @@ function Registration({ typeRegister }) {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "supabase-url": "https://srseiyeepchrklzxawsm.supabase.co/",
+          "supabase-url": "https://ligqdgmwtziqytxyqpvv.supabase.co/",
           "supabase-key":
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyc2VpeWVlcGNocmtsenhhd3NtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc5ODE2NjgsImV4cCI6MjAzMzU1NzY2OH0.WfcrXLHOj1aDt36XJ873SP8syg4I41rJgE_uV_X1vkU",
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpZ3FkZ213dHppcXl0eHlxcHZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY3NTE0MDQsImV4cCI6MjA1MjMyNzQwNH0.qHmECzoG1DfCs9zjirzwRzmp2V9OhBsKUr6tgnDCCq8",
         },
         body: JSON.stringify({
           email_address: formData.email,
@@ -63,9 +63,9 @@ function Registration({ typeRegister }) {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "supabase-url": "https://srseiyeepchrklzxawsm.supabase.co/",
+            "supabase-url": "https://ligqdgmwtziqytxyqpvv.supabase.co/",
             "supabase-key":
-              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyc2VpeWVlcGNocmtsenhhd3NtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc5ODE2NjgsImV4cCI6MjAzMzU1NzY2OH0.WfcrXLHOj1aDt36XJ873SP8syg4I41rJgE_uV_X1vkU",
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpZ3FkZ213dHppcXl0eHlxcHZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY3NTE0MDQsImV4cCI6MjA1MjMyNzQwNH0.qHmECzoG1DfCs9zjirzwRzmp2V9OhBsKUr6tgnDCCq8",
           },
           body: JSON.stringify({
             fname: formData.fname,
